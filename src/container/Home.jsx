e1f67d5d06d7903d7fc6a67505c6b90600f2fd8f
Cristian <cristianjuvinao801@gmai.com>
2023-01-31 21:06:49 -0500

    logicalchange

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -4,13 +4,21 @@ import { Character } from '../components/Characters'
 
 export const Home = () => {
     const [characters, SetCharacters] = useState([])
+    const [open, setopen] = useState(false)
+    
+    const [dataProfile, setdataProfile] = useState({})
+
     useEffect(() => {
         fetch(process.env.REACT_APP_API_LINK)
             .then(response => response.json())
             .then(data => SetCharacters(data.results))
             .catch(error => console.error(error))
     }, [])
-
+    const handleOpenModal = (characters) => {
+        setopen(true)
+        setdataProfile(characters)
+    }
+    console.log(dataProfile)
     return (
         <>
             <div className='charactersContainer'>
@@ -19,6 +27,7 @@ export const Home = () => {
                         characters.map((characters) => {
                             return (
                                 <Character
+                                    onClick={() => { handleOpenModal(characters) }}
                                     gender={characters.gender}
                                     image={characters.image}
                                     name={characters.name}
@@ -27,6 +36,11 @@ export const Home = () => {
                                 />
                             )
                         })
+                    }
+                    {open &&
+                        <div className='modal'>
+                            {dataProfile.name}
+                        </div>
                     }
                 </div>
             </div>
